5dc55546fc19cca117a38c63129d904adf503459
Wanderley01 <edioneto00@gmail.com>
2024-07-13 15:49:22 -0300

    Header da aplicação completa

--- hombridade/src/screens/homepage.jsx
+++ hombridade/src/screens/homepage.jsx
@@ -12,15 +12,18 @@ function HomePage() {
         <div name="barrademenu" className='menu'>
             
                 <div className='logo'>
-                    <img src="" alt="" />
+                    <img src="../src/assets/img/logo.png" width="80px" />
                 </div>
+                
+                
+                <h1 className='titulo'>HOMBRIDADE</h1>
 
                 <div className='itensmenu'>
                     <ul>
-                        <li><a href="./">Home</a></li>
-                        <li><a href="#">Hombridade</a></li>
-                        <li><a href="#">Blog</a></li>
-                        <li><a href="./Login">Login</a></li>
+                        <li><a href="./" className='opcao'>Home</a></li>
+                        <li><a href="#" className='opcao'>Hombridade</a></li>
+                        <li><a href="#" className='opcao'>Blog</a></li>
+                        <li><a href="./Login" className='opcao'>Login</a></li>
                     </ul>
                 </div>
 
